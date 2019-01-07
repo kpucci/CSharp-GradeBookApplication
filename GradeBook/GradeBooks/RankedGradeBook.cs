81b12716201f864b544f5b35fff98a876d9c7c31
Katie Pucci <KPucci@aerotech.com>
2019-01-07 09:18:30 -0500

    Override RankedGradeBook's GetLetterGrade
    Attempt #1

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,12 +1,48 @@
 ﻿using GradeBook.Enums;
+using System;
+using System.Collections.Generic;
 
 namespace GradeBook.GradeBooks
 {
     public class RankedGradeBook : BaseGradeBook
     {
+
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+            Students.Sort();
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5) throw new InvalidOperationException();
+
+            int cutoffNum = Students.Count / 5;
+
+            int count = 0;
+
+            foreach(Student s in Students)
+            {
+                if (s.AverageGrade == averageGrade)
+                    break;
+                else if (s.AverageGrade > averageGrade)
+                    count++;
+            }
+
+            int gradeNum = count / cutoffNum;
+            switch (gradeNum)
+            {
+                case 0:
+                    return 'A';
+                case 1:
+                    return 'B';
+                case 2:
+                    return 'C';
+                case 3:
+                    return 'D';                 
+            }
+
+            return 'F';
         }
     }
 }
--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using GradeBook.UserInterfaces;
+using GradeBook.Enums;
+using GradeBook.GradeBooks;
 
 namespace GradeBook
 {
@@ -7,6 +9,8 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
+            //testRankedGradeBook();
+
             Console.WriteLine("#=======================#");
             Console.WriteLine("# Welcome to GradeBook! #");
             Console.WriteLine("#=======================#");
@@ -17,6 +21,58 @@ namespace GradeBook
             Console.WriteLine("Thank you for using GradeBook!");
             Console.WriteLine("Have a nice day!");
             Console.Read();
+        }
+
+        static void testRankedGradeBook()
+        {
+            BaseGradeBook gradebook = new RankedGradeBook("rankedTest");
+
+            Student katie = new Student("Katie", StudentType.Standard, EnrollmentType.Campus);
+            Student vincent = new Student("Vincent", StudentType.Standard, EnrollmentType.Campus);
+            Student brian = new Student("Brian", StudentType.Standard, EnrollmentType.Campus);
+            Student tommy = new Student("Tommy", StudentType.Standard, EnrollmentType.Campus);
+            Student phillip = new Student("Phillip", StudentType.Standard, EnrollmentType.Campus);
+            Student andrew = new Student("Andrew", StudentType.Standard, EnrollmentType.Campus);
+
+            gradebook.AddStudent(katie);
+            gradebook.AddStudent(vincent);
+            gradebook.AddStudent(brian);
+            gradebook.AddStudent(tommy);
+
+            gradebook.AddGrade("Katie", 100.0);
+            gradebook.AddGrade("Vincent", 90.0);
+            gradebook.AddGrade("Brian", 80.0);
+            gradebook.AddGrade("Tommy", 70.0);
+
+            try
+            {
+                gradebook.GetLetterGrade(95.0);
+                Console.WriteLine("Test 1 failed");
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine("Test 1 passed");
+            }
+
+            gradebook.AddStudent(phillip);
+            gradebook.AddGrade("Phillip", 60.0);
+            gradebook.AddStudent(andrew);
+            gradebook.AddGrade("Andrew", 100.0);
+
+            try
+            {
+                char grade = gradebook.GetLetterGrade(95.0);
+                Console.WriteLine(grade);
+                if(grade != 'C')
+                    Console.WriteLine("Test 2 failed");
+                else
+                    Console.WriteLine("Test 2 passed");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Test 2 failed");
+            }
+
         }
     }
 }
